7c1ed30e6e6334c9c608492a6b50f25a43260b2b
jack <jryansaunders@gmail.com>
2019-03-25 21:06:45 +0000

    start files

---
+++ header.php
@@ -0,0 +1,48 @@
+<html class="no-js" lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>PHP Example</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">   
+    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    
+    <link rel="stylesheet" type="text/css" href="style.css">
+    
+</head>
+    
+<header>
+      <p>header</p>  
+    
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">WebSiteName</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Page 1</a></li>
+      <li><a href="#">Page 2</a></li>
+      <li><a href="#">Page 3</a></li>
+    </ul>
+  </div>
+</nav>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+</header>
+    
+<body>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+include 'header.php'; ?>
+
+
+
+
+
+Welcome to PHP programming language.
+
+
+
+
+
+<?php 
+include 'footer.php';
+?>
